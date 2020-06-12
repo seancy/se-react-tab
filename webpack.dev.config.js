c726e2d366becc441766212425b36e049b15b496
Sean <se.zhang@learning-tribes.com>
2020-06-12 11:16:34 +0800

    update dependents. add host to devserver to support accessing from other computer.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -6,6 +6,7 @@ const path = require('path')
 module.exports = Merge.smart(commonConfig, {
     devtool: 'source-map',
     devServer:{
+        host: '0.0.0.0',
         port: 8085,
         contentBase: path.join(__dirname, 'dist'),
         watchOptions: {
